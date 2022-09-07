fb4255ea895dce014c20b10e514cd721218fe340
Augusto-Camani <augustocamani@gmail.com>
2022-09-07 14:41:43 -0300

    Modificación Intervalos

--- Tp estadistica.py
+++ Tp estadistica.py
@@ -22,6 +22,9 @@ listaValores = [listaDatos[0]]
 #Guardamos el primer valor de la lista en un auxiliar.
 auxiliar = listaDatos[0]
 
+#Datos agrupados por intervalos.
+listaDatosAgrupados = list(range(55000, 127000 + 1 , 6000))
+
 #Recorremos la lista y añadimos los elementos que no se repiten a una nueva lista.
 for i in listaDatos:
     if i != auxiliar:
@@ -29,7 +32,7 @@ for i in listaDatos:
         auxiliar = i
 
 #Rellenamos las frecuencias de los valores y creamos una variable para las frecuencias.
-listaFrecuencia = [0]
+listaFrecuenciasAbsolutas = [0]
 
 #Creamos una variable iterador. Volvimos a valorizar el auxiliar con el primer dato de datosLista.
 #Iteramos datosLista y para cada uno de sus elemento si son distintos al auxiliar, en la locación que tiene listaFrecuencia con índice(iterador) sumamos uno a la frecuencia.
@@ -40,19 +43,38 @@ for i in listaDatos:
     if i != auxiliar:
         auxiliar = i
         iterador += 1
-        listaFrecuencia.append(0)
-    listaFrecuencia[iterador] += 1
+        listaFrecuenciasAbsolutas.append(0)
+    listaFrecuenciasAbsolutas[iterador] += 1
 
-print(len(listaValores))
-print(len(listaFrecuencia))
+print("La cantidad de elementos no repetidos son:", len(listaValores))
+print("La catidad de datos individuales, que aparecen 1 o más veces son:",len(listaFrecuenciasAbsolutas))
 
 #Corroboramos que la sumatoria de frecuencias sea igual a el número de la muestra total.
-print(muestraTotal)
-print(sum(listaFrecuencia))
+print("La muestra es de:",muestraTotal)
+print("La sumatoria de frecuencias es:",sum(listaFrecuenciasAbsolutas))
 
-df = pd.DataFrame(list(zip(listaValores,listaFrecuencia)), columns=["Valores", "Frecuencia"])
-print(df)
+#Cálculo de frecuencias absoluta acumulada.
+listaFrecuenciasAbsAcum = []
+auxiliar = 0
+for i in listaFrecuenciasAbsolutas:
+    listaFrecuenciasAbsAcum.append(i + auxiliar)
+    auxiliar += i
 
+#Cálculos de frecuencias relativas y frecuencias relativas acumuladas.
+listaFrecuenciasRelativas = []
+listaFrecuenciasRelAcum = []
+
+for i in listaFrecuenciasAbsolutas:
+    listaFrecuenciasRelativas.append(round(i / muestraTotal, 5))
+
+for i in listaFrecuenciasAbsAcum:
+    listaFrecuenciasRelAcum.append(round(i / muestraTotal , 5))
+ 
+#Valores mínimo y máximo.
+print("El valor mínimo es:", listaValores[0])
+print("El valor máximo es:", listaValores[-1])
+print("Datos agrupados de a 6000 km.")
+print(listaDatosAgrupados)
 
 #Cierre del archivo.
 miArchivo.close()
